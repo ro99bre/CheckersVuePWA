8b35d2532864535d1b9d7aec1c4af2593e2730c0
Robert <robert.breuer@htwg-konstanz.de>
2021-12-18 13:14:21 +0100

    First Implementation of Menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id=app>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="startscreen">Checkers</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home</router-link>
+              <!--<router-link to="/">Home</router-link>-->
+              <!--<a class="nav-link" href="tutorial">Tutorial</a>-->
+            </li>
+
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/test">Test</router-link>
+            </li>
+
+            <!--<li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="currentGame" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Aktuelles Spiel
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="currentGame">Neu laden</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="newGame">Neues Spiel erstellen</a>
+              </div>
+            </li>-->
+          </ul>
+        </div>
+      </nav>
+      <router-view/>
   </div>
 </template>
 
