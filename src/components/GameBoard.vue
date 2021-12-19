263bbece09a815178f822367352ca2bfb0fea446
Robert <robert.breuer@htwg-konstanz.de>
2021-12-19 18:55:55 +0100

    Refactored styling of Game Vue

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -154,18 +154,4 @@ export default {
 
 </script>
 
-<style scoped>
-  .field{
-    float: left;
-    width: 100%;
-    height: 100%;
-  }
-  .GameBoard{
-    width: 100%;
-    max-width: 960px;
-  }
-  .btn-toolbar{
-    margin: 15px;
-    margin-left: 0px;
-  }
-</style>
+
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -26,3 +26,19 @@ export default {
   }
 }
 </script>
+
+<style>
+  .field{
+    float: left;
+    width: 100%;
+    height: 100%;
+  }
+  .GameBoard{
+    width: 100%;
+    max-width: 960px;
+  }
+  .btn-toolbar{
+    margin: 15px;
+    margin-left: 0px;
+  }
+</style>
