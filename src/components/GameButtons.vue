260714682fbec7d1396bfa3fd40494ff1b01e92d
Robert <robert.breuer@htwg-konstanz.de>
2021-12-19 18:56:57 +0100

    Added GameButtons template

--- src/components/GameButtons.vue
+++ src/components/GameButtons.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1>Test</h1>
+    <div class="btn-toolbar" role="toolbar">
+      <div class="d-grid gap-2 d-md-block col-12 mx-auto" role="group">
+        <button type="button" class="btn btn-danger" id="undo-button">Undo</button>
+        <button type="button" class="btn btn-secondary" id="redo-button">Redo</button>
+        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#loadModal">Load</button>
+        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#saveModal">Save</button>
+      </div>
+    </div>
 </template>
