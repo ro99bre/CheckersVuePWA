3b67f5b0535ea26fd80efe87ee2089840dc7732e
Github Action <50114800+ro99bre@users.noreply.github.com>
2021-12-30 05:39:49 +0000

    Deployment from Job-Nr 1 triggered by Commit e88b702f91e7df28950a8eeab862051a76333ee5

---
+++ precache-manifest.836726ddac374984a65d04459b862f7c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a46c10b73199460910",
+    "url": "/CheckersVuePWA/css/chunk-253d1c6f.f11174be.css"
+  },
+  {
+    "revision": "806fe8cff1328823a725",
+    "url": "/CheckersVuePWA/css/chunk-278997fb.c2447c90.css"
+  },
+  {
+    "revision": "2b54be51353bb1dc3d8b",
+    "url": "/CheckersVuePWA/css/chunk-3e384cc2.7b202c34.css"
+  },
+  {
+    "revision": "5a99425c2c90955feaee",
+    "url": "/CheckersVuePWA/css/chunk-vendors.f75dd688.css"
+  },
+  {
+    "revision": "ff65d9510c69b7f56fa8ee3fa7fdc1e4",
+    "url": "/CheckersVuePWA/img/Play-Checkers-Step-14-Version-6.360p.ff65d951.gif"
+  },
+  {
+    "revision": "0719b3409843926332ea1dc6673d1308",
+    "url": "/CheckersVuePWA/img/chessboard.0719b340.jpg"
+  },
+  {
+    "revision": "466e7df9ede9bbaf874553ffd17608e8",
+    "url": "/CheckersVuePWA/index.html"
+  },
+  {
+    "revision": "e69e51e757262a339599",
+    "url": "/CheckersVuePWA/js/app.9944b407.js"
+  },
+  {
+    "revision": "08a46c10b73199460910",
+    "url": "/CheckersVuePWA/js/chunk-253d1c6f.fa334506.js"
+  },
+  {
+    "revision": "806fe8cff1328823a725",
+    "url": "/CheckersVuePWA/js/chunk-278997fb.2f06b67d.js"
+  },
+  {
+    "revision": "2b54be51353bb1dc3d8b",
+    "url": "/CheckersVuePWA/js/chunk-3e384cc2.ca7cb576.js"
+  },
+  {
+    "revision": "5a99425c2c90955feaee",
+    "url": "/CheckersVuePWA/js/chunk-vendors.79cbfba6.js"
+  },
+  {
+    "revision": "983037f4f96bea73b1ad7a99b62ce305",
+    "url": "/CheckersVuePWA/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CheckersVuePWA/robots.txt"
+  }
+]);
