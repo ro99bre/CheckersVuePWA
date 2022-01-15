f2954fe2cf5996d198a91811b7c5ffbcb63478d6
Robert <robert.breuer@htwg-konstanz.de>
2022-01-15 11:19:06 +0100

    Adapted Startpage link URLs

--- src/views/Startscreen.vue
+++ src/views/Startscreen.vue
@@ -4,15 +4,15 @@
       <img src="../assets/startscreen/chessboard.jpg" alt="checkers startscreen" usemap="#chessmap"/>
       
       <map name="chessmap">
-        <area target="" alt="newGame" title="newGame" href="http://localhost:3000/game" coords="377,563,54" shape="circle">
-        <area target="" alt="tutorial" title="tutorial" href="http://localhost:3000/tutorial" coords="1332,559,52" shape="circle">
-        <area target="" alt="play" title="play" href="http://localhost:3000/newGame" coords="572,593,440,532" shape="rect">
-        <area target="" alt="tutorial" title="tutorial" href="http://localhost:3000/tutorial" coords="1404,531,1602,585" shape="rect">
+        <area target="" alt="newGame" title="newGame" href="https://ro99bre.github.io/CheckersVuePWA/#/game" coords="377,563,54" shape="circle">
+        <area target="" alt="tutorial" title="tutorial" href="https://ro99bre.github.io/CheckersVuePWA/#/tutorial" coords="1332,559,52" shape="circle">
+        <area target="" alt="play" title="play" href="https://ro99bre.github.io/CheckersVuePWA/#/newGame" coords="572,593,440,532" shape="rect">
+        <area target="" alt="tutorial" title="tutorial" href="https://ro99bre.github.io/CheckersVuePWA/#/tutorial" coords="1404,531,1602,585" shape="rect">
         <area target="" alt="github project" title="github" href="https://github.com/ro99bre/CheckersWebApp" coords="961,419,22" shape="circle">
       </map>
       <div class="ebuttons">
-        <a href="http://localhost:3000/game" id="playbutton">PLAY</a>
-        <a href="http://localhost:3000/tutorial" id="tutorialbutton">TUTORIAL</a>
+        <a href="https://ro99bre.github.io/CheckersVuePWA/#/game" id="playbutton">PLAY</a>
+        <a href="https://ro99bre.github.io/CheckersVuePWA/#/tutorial" id="tutorialbutton">TUTORIAL</a>
         <a href="https://github.com/ro99bre/CheckersWebApp" id="githubbutton">Github Project</a>
       </div>
 
